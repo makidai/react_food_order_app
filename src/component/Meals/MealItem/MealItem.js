d51069c462a0217f0c03b4c2aee486f9c7fbd05a
Ryodai Makino <davinci0327@icloud.com>
2021-06-06 00:15:30 +0900

    add Form

--- src/component/Meals/MealItem/MealItem.js
+++ src/component/Meals/MealItem/MealItem.js
@@ -1,8 +1,8 @@
+import MealItemForm from "./MealItemForm";
 import classes from "./MealItem.module.css";
 
 const MealItem = (props) => {
-
-    const price = `$${props.price.toFixed(2)}`
+    const price = `$${props.price.toFixed(2)}`;
     return (
         <li className={classes.meal}>
             <div>
@@ -11,7 +11,7 @@ const MealItem = (props) => {
                 <div className={classes.price}>{price}</div>
             </div>
             <div>
-
+                <MealItemForm id={props.id} />
             </div>
         </li>
     );
