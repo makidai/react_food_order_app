9395f7c73a5096edc79387bd147498cdf4a7a62a
Ryodai Makino <davinci0327@icloud.com>
2021-06-06 09:51:56 +0900

    add Modal show state

--- src/component/UI/Modal.js
+++ src/component/UI/Modal.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 import classes from "./Modal.module.css";
 
 const Backdrop = (props) => {
-    return <div className={classes.backdrop} />;
+    return <div className={classes.backdrop} onClick={props.onClose}/>;
 };
 
 const ModalOverlay = (props) => {
@@ -20,7 +20,7 @@ const PortalElement = document.getElementById("overlays");
 const Modal = (props) => {
     return (
         <Fragment>
-            {ReactDOM.createPortal(<Backdrop />, PortalElement)}
+            {ReactDOM.createPortal(<Backdrop onClose={props.onClose}/>, PortalElement)}
             {ReactDOM.createPortal(
                 <ModalOverlay>{props.children}</ModalOverlay>,
                 PortalElement
